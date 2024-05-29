ce500bf02c9d759f1aab27fa5098b5e9fce35112
tal waserman <talwaserman@gmail.com>
2024-05-29 18:06:28 +0300

    adding baseHash and collection address

--- internal/marketplace/model/model.go
+++ internal/marketplace/model/model.go
@@ -41,19 +41,21 @@ type (
 	Item struct {
 		bun.BaseModel `bun:"table:items"`
 
-		ID           int64          `bun:"id,pk,autoincrement" json:"id"`
-		CollectionID int64          `bun:"collection_id,notnull" json:"collection_id"`
-		Name         string         `bun:"name" json:"name"`
-		Description  string         `bun:"description" json:"description"`
-		ImageID      int64          `bun:"image_id" json:"image_id"`
-		FiatPrice    float64        `bun:"fiat_price" json:"fiat_price"`
-		TotalAmount  int64          `bun:"total_amount" json:"total_amount"`
-		ListedAmount int64          `bun:"listed_amount" json:"listed_amount"`
-		Attributes   map[string]any `bun:"attributes" json:"attributes"`
-		CreatedAt    time.Time      `bun:"created_at,default:current_timestamp" json:"created_at"`
-		DeletedAt    *time.Time     `bun:"deleted_at,soft_delete" json:"-"`
-		NetworkID    *string        `bun:"-" json:"network_id,omitempty"`
-		ChainID      *int64         `bun:"-" json:"chain_id,omitempty"`
+		ID                int64          `bun:"id,pk,autoincrement" json:"id"`
+		CollectionID      int64          `bun:"collection_id,notnull" json:"collection_id"`
+		Name              string         `bun:"name" json:"name"`
+		Description       string         `bun:"description" json:"description"`
+		ImageID           int64          `bun:"image_id" json:"image_id"`
+		FiatPrice         float64        `bun:"fiat_price" json:"fiat_price"`
+		TotalAmount       int64          `bun:"total_amount" json:"total_amount"`
+		ListedAmount      int64          `bun:"listed_amount" json:"listed_amount"`
+		Attributes        map[string]any `bun:"attributes" json:"attributes"`
+		CreatedAt         time.Time      `bun:"created_at,default:current_timestamp" json:"created_at"`
+		DeletedAt         *time.Time     `bun:"deleted_at,soft_delete" json:"-"`
+		NetworkID         *string        `bun:"-" json:"network_id,omitempty"`
+		ChainID           *int64         `bun:"-" json:"chain_id,omitempty"`
+		BaseHash          *string        `bun:"-" json:"base_hash,omitempty"`
+		CollectionAddress *string        `bun:"-" json:"address,omitempty"`
 	}
 )
 
